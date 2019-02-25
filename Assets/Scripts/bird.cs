1dc4d414794babbc18f2d05d64304ea05096df8b
junsoo <junsoolee@kaist.ac.kr>
2019-02-26 01:12:16 +0900

    Making A Flappy Bird Style Game - 3까지 진행
    https://unity3d.com/kr/learn/tutorials/topics/2d-game-creation/bird-script?playlist=17093

---
+++ Assets/Scripts/bird.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bird : MonoBehaviour
+{
+    public float upforce = 200f;
+
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D> ();
+        rb2d.velocity = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isDead)
+        {
+            if(Input.GetMouseButtonDown (0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upforce));
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isDead = true;
+    }
+}
